5d26d11ff65ee50795aceb2ecc06c58c6163eafb
bency <bency80097@gmail.com>
2015-03-25 01:02:08 +0800

    first commit

---
+++ matrix.php
@@ -0,0 +1,113 @@
+<?php
+class Pixel
+{
+    static public $alphabet = [
+        ' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ',
+        ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+        ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '
+    ];
+    public $color_code = 32;  // 31 ~ 37
+    public $bright = 1;
+    public $dot = '';
+
+    public function __construct()
+    {
+        $this->setRandomAlphabet();
+    }
+
+    public function getAscii()
+    {
+        return "\e[{$this->bright};{$this->color_code}m{$this->dot}\e[0m";
+    }
+
+    public function setRandomAlphabet()
+    {
+        $this->bright = rand() % 2;
+        $rand = rand() % count(self::$alphabet);
+        $this->dot = self::$alphabet[$rand];
+    }
+}
+
+class Row
+{
+    public $cell = [];
+    public $width;
+    public function __construct($width)
+    {
+        $this->width = $width;
+        for ($i = 0; $i < $width; $i++) {
+            $this->cell[$i] = new Pixel;
+        }
+    }
+
+    public function display()
+    {
+        for ($i = 0; $i < $this->width; $i++) {
+            echo $this->cell[$i]->getAscii();
+        }
+        echo PHP_EOL;
+    }
+}
+
+class Layout
+{
+    public $row = [];
+    public $heigh = 0;
+    public $width = 0;
+    public function __construct($heigh, $width)
+    {
+        $this->heigh = $heigh;
+        $this->width = $width;
+    }
+
+    public function display()
+    {
+        echo "\e[H\e[J";
+        $this->dropRow();
+        $this->addRow();
+        $max = count($this->row);
+        for ($heigh = $max - 1; $heigh >= 0; $heigh--) {
+            $this->row[$heigh]->display();
+        }
+        sleep(1);
+    }
+
+    private function addRow()
+    {
+        $new = new Row($this->width);
+        if ($last_key = count($this->row)) {
+            $last = $this->row[$last_key - 1];
+            foreach ($new->cell as $key => &$cell) {
+                if ($last->cell[$key]->dot == '') {
+                    $cell->color_code = 37;
+                    $cell->bright = 1;
+                }
+            }
+        } else {
+            foreach ($new->cell as $key => &$cell) {
+                $cell->color_code = 37;
+                $cell->bright = 1;
+            }
+        }
+        $this->row[] = $new;
+    }
+
+    private function dropRow()
+    {
+        if (count($this->row) < $this->heigh) {
+            return;
+        }
+        array_shift($this->row);
+    }
+}
+// 取得當前長寬
+exec('tput cols', $arr);
+exec('tput lines', $arr);
+$width = $arr[0];
+$heigh = $arr[1] - 1;
+$layout = new Layout($heigh, $width);
+
+while(1) {
+    $layout->display();
+}
