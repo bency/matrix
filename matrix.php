4e6c6ab6cee0de25036a0047538e0306deec92e2
bency <bency80097@gmail.com>
2015-07-06 02:46:30 +0800

    讓 layout 負責整個顯示的流程

--- matrix.php
+++ matrix.php
@@ -7,29 +7,4 @@ $width = $heigh = 0;
 
 
 $layout = new Matrix\Layout();
-
-while(1) {
-
-    $c = fread(STDIN, 1);
-    if (in_array($c, ['='])) {
-        $layout->increaseSleep();
-    } elseif (in_array($c, ['-'])) {
-        $layout->decreaseSleep();
-    }
-
-    // 重設環境變數
-    unset($envi_param);
-    exec('tput cols', $envi_param);
-    exec('tput lines', $envi_param);
-
-    if ($envi_param[0] != $width) {
-        $width = $envi_param[0];
-        $layout->setWidth($width);
-    }
-
-    if (($envi_param[1]) != $heigh) {
-        $heigh = $envi_param[1];
-        $layout->setHeight($heigh);
-    }
-    $layout->display();
-}
+$layout->run();
