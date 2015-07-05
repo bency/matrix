e89f28d9d5f82133dd369c3e1f64530dceddd19a
bency <bency80097@gmail.com>
2015-07-06 02:48:43 +0800

    remove useless variable

--- matrix.php
+++ matrix.php
@@ -3,8 +3,5 @@ include __DIR__ . "/src/Layout.php";
 include __DIR__ . "/src/Row.php";
 include __DIR__ . "/src/Pixel.php";
 
-$width = $heigh = 0;
-
-
 $layout = new Matrix\Layout();
 $layout->run();
