6293c738bd32c05cbbca6de56d318298f1fb904b
bency <bency80097@gmail.com>
2016-01-19 03:20:08 +0800

    add Pixel test

---
+++ tests/src/PixelTest.php
@@ -0,0 +1,51 @@
+<?php
+use Matrix\Pixel;
+class PixelTest extends PHPUnit_Framework_TestCase
+{
+    public function testColorSet()
+    {
+        $target = new Pixel;
+        $target->setColorSet();
+        $color = $target->getColor();
+        $expect = Pixel::$color_256[0][0];
+        $this->assertEquals($expect, $color);
+    }
+
+    public function testSecondColor()
+    {
+        $target = new Pixel;
+        $target->setColorSet();
+        $target->nextColor();
+        $color = $target->getColor();
+        $expect = Pixel::$color_256[0][1];
+        $this->assertEquals($expect, $color);
+    }
+
+    public function testNextColorSet()
+    {
+        $target = new Pixel;
+        $target->setColorSet();
+        $target->nextColorSet();
+        $target->nextColor();
+        $target->nextColor();
+        $target->nextColor();
+        $target->nextColor();
+        $target->nextColor();
+        $target->nextColor();
+        $target->nextColor();
+        $target->nextColor();
+        $color = $target->getColor();
+        $expect = Pixel::$color_256[1][0];
+        $this->assertEquals($expect, $color);
+    }
+
+    public function testDelayNextColorSet()
+    {
+        $target = new Pixel;
+        $target->setColorSet();
+        $target->nextColorSet();
+        $color = $target->getColor();
+        $expect = Pixel::$color_256[0][0];
+        $this->assertEquals($expect, $color);
+    }
+}
